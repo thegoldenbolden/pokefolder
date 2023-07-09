534d7e57cac769b2f5379428fd42fc13f756f809
Jacob Bolden <60676197+thegoldenbolden@users.noreply.github.com>
2023-07-09 13:40:53 -0500

    feat(icons): add lucide icons

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -1,99 +1,97 @@
-type IconProps = React.SVGProps<SVGSVGElement>;
+import type { SVGProps } from "react";
+export type Icon = (props: SVGProps<SVGSVGElement>) => JSX.Element;
 
-const BaseIcon = (props: IconProps) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      viewBox="0 0 16 16"
-      {...props}
-    >
-      {props.children}
-    </svg>
-  );
+const BaseIcon: Icon = (props) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="currentColor"
+			viewBox="0 0 24 24"
+			{...props}
+		>
+			{props.children}
+		</svg>
+	);
 };
 
-export function LightbulbOffIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="light mode" {...props}>
-      <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z" />
-    </BaseIcon>
-  );
+export const Moon: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="moon icon" {...props}>
+			<path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+		</BaseIcon>
+	)
+};
+
+export const Sun: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="sun icon" {...props}>
+			<circle cx="12" cy="12" r="4" /><path d="M12 2v2" /><path d="M12 20v2" /><path d="m4.93 4.93 1.41 1.41" /><path d="m17.66 17.66 1.41 1.41" /><path d="M2 12h2" /><path d="M20 12h2" /><path d="m6.34 17.66-1.41 1.41" /><path d="m19.07 4.93-1.41 1.41" />
+		</BaseIcon>
+	)
 }
 
-export function LightbulbOnIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="dark mode" {...props}>
-      <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z" />
-    </BaseIcon>
-  );
+export const Search: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="search icon" {...props}>
+			<circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" />
+		</BaseIcon>
+	)
 }
 
-export function SearchIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="search icon" {...props}>
-      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-    </BaseIcon>
-  );
+export const SearchX: Icon = (props) => {
+	return (<BaseIcon aria-label="reset search" {...props}>
+		<path d="m13.5 8.5-5 5" /><path d="m8.5 8.5 5 5" /><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" />
+	</BaseIcon>)
 }
 
-export function CalendarIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="calendar icon" {...props}>
-      <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-    </BaseIcon>
-  );
+export const Calendar: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="calendar icon" {...props}>
+			<rect width="18" height="18" x="3" y="4" rx="2" ry="2" /><line x1="16" x2="16" y1="2" y2="6" /><line x1="8" x2="8" y1="2" y2="6" /><line x1="3" x2="21" y1="10" y2="10" />
+		</BaseIcon>
+	)
+};
+
+export const X: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="x icon" {...props}>
+			<path d="M18 6 6 18" /><path d="m6 6 12 12" />
+		</BaseIcon>
+	)
 }
 
-export function CloseIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="close icon" {...props}>
-      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-    </BaseIcon>
-  );
+export const Filter: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="filter icon"
+			{...props}>
+			<line x1="21" x2="14" y1="4" y2="4" /><line x1="10" x2="3" y1="4" y2="4" /><line x1="21" x2="12" y1="12" y2="12" /><line x1="8" x2="3" y1="12" y2="12" /><line x1="21" x2="16" y1="20" y2="20" /><line x1="12" x2="3" y1="20" y2="20" /><line x1="14" x2="14" y1="2" y2="6" /><line x1="8" x2="8" y1="10" y2="14" /><line x1="16" x2="16" y1="18" y2="22" />
+		</BaseIcon>
+	)
 }
 
-export function FilterIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="filer icon" {...props}>
-      <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
-    </BaseIcon>
-  );
+export const ChevronUp: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="chevron up icon"
+			{...props}>
+			<path d="m18 15-6-6-6 6" />
+		</BaseIcon>
+	)
 }
 
-export function ArrowUpIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="arrow up icon" {...props}>
-      <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
-    </BaseIcon>
-  );
+export const ChevronDown: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="chevron down icon" {...props}>
+			<path d="m6 9 6 6 6-6" />
+		</BaseIcon>
+	)
 }
 
-export function ChevronRightIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="chevron icon" {...props}>
-      <path
-        fillRule="evenodd"
-        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-      />
-    </BaseIcon>
-  );
-}
-
-export function GithubIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="github icon" {...props}>
-      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-    </BaseIcon>
-  );
-}
-
-export function GlobeIcon(props: IconProps) {
-  return (
-    <BaseIcon aria-label="personal website" {...props}>
-      <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z" />
-    </BaseIcon>
-  );
+export const Check: Icon = (props) => {
+	return (
+		<BaseIcon aria-label="check icon" {...props}>
+			<polyline points="20 6 9 17 4 12" />
+		</BaseIcon>
+	)
 }
