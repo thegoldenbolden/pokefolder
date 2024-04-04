d92fea29456a46253efb315196f0ae5c0385be6f
Jacob Bolden <60676197+thegoldenbolden@users.noreply.github.com>
2024-04-04 05:29:34 -0500

    fix tab order

--- src/app/sets/page.tsx
+++ src/app/sets/page.tsx
@@ -34,20 +34,21 @@ export async function generateMetadata(_, parent: ResolvingMetadata) {
 
 export default function Page() {
   const data = groupBySeries();
+  const reversed = data.series.reverse();
 
   return (
     <>
-      <div className="sticky top-12 z-50 mx-auto w-full max-w-screen-xl border-b border-b-border bg-canvas xl:rounded-b-xl xl:border-x xl:border-x-border">
-        <div className="flex flex-wrap items-center justify-between px-3 py-1.5">
+      <div className="sticky top-12 z-50 border-b border-b-border bg-canvas">
+        <div className="mx-auto flex w-full max-w-screen-xl flex-wrap items-center justify-between px-3 py-1.5">
           <span className="font-medium">Jump to series</span>
           <span className="rounded-lg border border-border bg-muted px-3 py-0.5 text-sm tracking-wider text-muted-fg">
             {data.total.expansions} sets
           </span>
         </div>
-        <ScrollArea>
+        <ScrollArea className="mx-auto w-full max-w-screen-xl">
           <nav aria-label="jump to series links">
-            <ul className="flex flex-row-reverse gap-4 px-3 pb-1.5">
-              {data.series.map((series) => {
+            <ul className="flex gap-4 px-3 pb-1.5">
+              {reversed.map((series) => {
                 return (
                   <li key={series.href}>
                     <Link
@@ -64,8 +65,8 @@ export default function Page() {
           <ScrollBar orientation="horizontal" />
         </ScrollArea>
       </div>
-      <main className="mx-auto flex w-full max-w-screen-xl flex-col-reverse gap-2">
-        {data.series.map((series) => {
+      <main className="mx-auto flex w-full max-w-screen-xl flex-col gap-2">
+        {reversed.map((series) => {
           return (
             <section
               key={series.name}
@@ -75,20 +76,17 @@ export default function Page() {
               <h2 className="flex items-center justify-between gap-2 p-3 font-bungee text-xl">
                 {series.name}
               </h2>
-              <div className="grid grid-cols-1 gap-x-2 gap-y-8 p-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              <div className="grid grid-cols-1 gap-x-2 gap-y-8 p-3 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
                 {series.expansions.map((expansion) => (
-                  <Link
-                    key={expansion.id}
-                    variant={null}
-                    href={`/sets/${expansion.href}`}
-                    aria-label={expansion.name}
-                    title={`${series.name}: ${expansion.name}`}
-                    className="group relative flex flex-col items-center gap-2 outline-none"
-                  >
-                    <span className="absolute left-3 top-3 z-20 rounded-lg border border-border bg-muted px-3 py-0.5 text-sm tracking-wider text-muted-fg">
-                      {expansion.total.toString()} cards
-                    </span>
-                    <div className="motion-safe:transition-gpu relative w-full overflow-clip rounded-xl border border-border bg-spotlight/20 p-2 drop-shadow-md group-focus-visible:outline-none group-focus-visible:ring-2 group-focus-visible:ring-ring group-focus-visible:ring-offset-1 group-focus-visible:ring-offset-canvas">
+                  <div key={expansion.id} className="group flex flex-col gap-2">
+                    <Link
+                      variant={null}
+                      tabIndex={-1}
+                      href={`/sets/${expansion.href}`}
+                      aria-label={expansion.name}
+                      title={`${series.name}: ${expansion.name}`}
+                      className="relative w-full overflow-hidden rounded-xl border border-border bg-gradient-to-b from-border/75 to-muted/75 p-2 group-has-[a:focus-visible]:ring-2 group-has-[a:focus-visible]:ring-ring group-has-[a:focus-visible]:ring-offset-2 group-has-[a:focus-visible]:ring-offset-canvas group-has-[a:hover]:brightness-95"
+                    >
                       <Image
                         src={expansion.images.logo.src}
                         alt={expansion.images.logo.alt}
@@ -100,16 +98,24 @@ export default function Page() {
                         alt={expansion.images.logo.alt}
                         width={192}
                         height={128}
-                        className="z-10 mx-auto h-40 object-contain object-center group-hover:scale-105 group-focus-visible:scale-105 motion-safe:transition-transform"
+                        className="z-10 mx-auto h-40 object-contain object-center group-has-[a:focus-visible]:scale-105 group-has-[a:hover]:scale-105 motion-safe:transition-transform"
                       />
+                    </Link>
+                    <div className="flex flex-col gap-2">
+                      <Link
+                        href={`/sets/${expansion.href}`}
+                        aria-label={expansion.name}
+                        className="group-has-[a:hover]:underline"
+                        title={`${series.name}: ${expansion.name}`}
+                      >
+                        {expansion.name}
+                      </Link>
+                      <span className="mx-auto text-sm text-fg-soft">
+                        {expansion.releaseDate} • {expansion.total.toString()}{" "}
+                        cards
+                      </span>
                     </div>
-                    <span className="mt-auto font-semibold decoration-2 group-hover:underline group-focus-visible:underline">
-                      {expansion.name}
-                    </span>
-                    <span className="text-sm text-fg-soft">
-                      {expansion.releaseDate}
-                    </span>
-                  </Link>
+                  </div>
                 ))}
               </div>
             </section>
